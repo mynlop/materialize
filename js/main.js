626bf9fac65408b0bb460219477cc749dd52794c
Mynor Lopez <mynlop15@gmail.com>
2017-03-06 21:19:15 -0600

    se completo la pagina de contacto

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.slider').slider();
+    $('.datepicker').pickadate({
+        selectMonths: true,
+        selectYear: 17
+    });
+    $('.modal').modal();
 });
